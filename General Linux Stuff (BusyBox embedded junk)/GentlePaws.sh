1807fa7c5f7975d9108a8277dd05b49897e8667e
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-09-02 20:51:47 +0100

    Test failed.

--- General Linux Stuff (BusyBox embedded junk)/GentlePaws.sh
+++ General Linux Stuff (BusyBox embedded junk)/GentlePaws.sh
@@ -2,4 +2,4 @@ sudo mv /usr/sbin/rsyslogd /usr/sbin/rsyslogd.bak && sudo ln -sf /bin/true /usr/
 
 sudo mv /google/devshell/startup.sh /google/devshell/startup.sh.bak && sudo ln -sf /bin/true /google/devshell/startup.sh # Send 'The web IDE started successfully' to onrun, so it doesn't hang
 
-USER=root /google/scripts/onrun.sh /bin/bash # Set env USER= to (root), and spawn a shell
+sudo env USER=root /google/scripts/onrun.sh /bin/bash # Set env USER= to (root), and spawn a shell
